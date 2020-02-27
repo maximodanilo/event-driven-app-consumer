f47ed17deff28628302152f0e2d3c9304248a096
Danilo Almeida <Danilo.Almeida@cmic.ca>
2020-02-26 21:43:50 -0500

    Added subscription to event

--- src/main/java/com/example/eventdrivenappdemoconsumer/EventDrivenAppDemoConsumerApplication.java
+++ src/main/java/com/example/eventdrivenappdemoconsumer/EventDrivenAppDemoConsumerApplication.java
@@ -1,13 +1,20 @@
 package com.example.eventdrivenappdemoconsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(LoanProcessor.class)
 public class EventDrivenAppDemoConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventDrivenAppDemoConsumerApplication.class, args);
-	}
+	  public static final Logger log = LoggerFactory.getLogger(LoanCheckApplication.class);
+
+	  public static void main(String[] args) {
+	    SpringApplication.run(EventDrivenAppDemoConsumerApplication.class, args);
+	    log.info("The Loancheck Application has started...");
+	  }
 
 }
---
+++ src/main/java/com/example/eventdrivenappdemoconsumer/Statuses.java
@@ -0,0 +1,6 @@
+package com.example.eventdrivenappdemoconsumer;
+
+
+public enum Statuses {
+  APPROVED, DECLINED, PENDING, REJECTED
+}
