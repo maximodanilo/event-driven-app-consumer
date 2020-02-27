c7d59639c025af73de3025784e78f1fefbea3792
Danilo Almeida <Danilo.Almeida@cmic.ca>
2020-02-26 21:09:53 -0500

    First commit

---
+++ src/main/java/com/example/eventdrivenappdemoconsumer/EventDrivenAppDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventdrivenappdemoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenAppDemoConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenAppDemoConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventdrivenappdemoconsumer/EventDrivenAppDemoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventdrivenappdemoconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenAppDemoConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
